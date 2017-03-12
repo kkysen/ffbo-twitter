18a31c484a11cf05765c64b87a962c7742651e0f
Khyber Sen <kkysen@gmail.com>
2017-03-12 14:57:20 -0400

    started tweet.py authentication
    Signed-off-by: Khyber Sen <kkysen@gmail.com>

---
+++ tweet.py
@@ -0,0 +1,14 @@
+import tweepy
+
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+api = tweepy.API(auth)
+
+for k in tweepy.API.__dict__:
+    print k
